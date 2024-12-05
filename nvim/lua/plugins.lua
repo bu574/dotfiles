ac850ba1ea29cad303be2ff22ebcf0ba00511bca
bu574 <busta235@gmail.com>
2024-12-05 22:39:11 +0100

    updt: linter

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -42,20 +42,18 @@ packer.init({
 return packer.startup(function(use)
 	use ("wbthomason/packer.nvim") -- Have packer manage itself	
 
-    use 'williamboman/mason.nvim'   
-    
+    use 'williamboman/mason.nvim'
     use 'williamboman/mason-lspconfig.nvim'
-    
     use 'neovim/nvim-lspconfig'
 
     -- Hrsh7th Code Completion Suite
-    use 'hrsh7th/nvim-cmp' 
+    use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-nvim-lua'
     use 'hrsh7th/cmp-nvim-lsp-signature-help'
-    use 'hrsh7th/cmp-vsnip'                             
-    use 'hrsh7th/cmp-path'                              
-    use 'hrsh7th/cmp-buffer'                            
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/vim-vsnip'
 
     -- Dracula theme for styling
@@ -149,5 +147,8 @@ return packer.startup(function(use)
 
     --gruvbox theme
     use ('ellisonleao/gruvbox.nvim')
+
+    --nvim linter 
+    use 'mfussenegger/nvim-lint'
 
 end)
